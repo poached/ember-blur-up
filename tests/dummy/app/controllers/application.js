9a400213dffc76f8ae307487cab496d3664db038
Tristan Edwards <tristan.edwards@me.com>
2016-03-07 16:27:40 +0100

    Add demo url

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 import base64Image from './base64-image';
+import env from 'ember-blur-up/config/environment';
 
 export default Ember.Controller.extend({
   base64Data: base64Image,
@@ -11,7 +12,13 @@ export default Ember.Controller.extend({
   actions: {
     reloadImage() {
       let randomNumber = Math.random();
-      const originalURL = '/images/abstract.png';
+      let originalURL = '/images/abstract.png';
+
+      // Because of "ember-cli-github-pages"-bug
+      // See: https://github.com/poetic/ember-cli-github-pages/issues/11
+      if (env.environment === 'production') {
+        originalURL = `/ember-blur-up${originalURL}`;
+      }
 
       this.set('imageURL', `${originalURL}?t=${randomNumber}`);
     }
